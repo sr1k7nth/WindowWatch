138edb97555520542c84da049b9e52933b6eeb81
sr1k7nth <sr1kanth7.devsr1kanth7.dev@gmail.com>
2026-02-28 10:35:15 +0530

    Modified helper.py (handling config.json)

--- st_tracker/helper.py
+++ st_tracker/helper.py
@@ -2,18 +2,47 @@ import json, os
 
 def read():
   if os.path.exists("config.json"):
-    with open("config.json", "r") as f:
-      data = json.load(f)
-      return data["threshold_time"]
+    with open("config.json", "r+") as f:
+      try:
+        data = json.load(f)
+        return data["threshold_time"]
+      except (json.JSONDecodeError, KeyError):
+        f.seek(0)
+        f.truncate()
+        data = {
+          "threshold_time": 60
+        }
+        json.dump(data, f)
+        return 60
   else:
     print("config.json doesn't exits!")
+    with open("config.json", "x") as f:
+      data = {
+        "threshold_time": 60
+      }
+      json.dump(data, f)
     return 60
   
 
 def write(new_threshold):
   if os.path.exists("config.json"):
-    with open("config.json", "r") as f:
-      data = json.load(f)
-    with open("config.json", "w") as f:
-      data["threshold_time"] = new_threshold
+    with open("config.json", "r+") as f:
+      try:
+        data = json.load(f)
+        f.seek(0)
+        f.truncate()
+        data["threshold_time"] = new_threshold
+        json.dump(data, f)
+      except (json.JSONDecodeError, KeyError):
+        f.seek(0)
+        f.truncate()
+        data = {
+          "threshold_time": new_threshold
+        }
+        json.dump(data, f)
+  else:
+    with open("config.json", "x") as f:
+      data = {
+        "threshold_time": new_threshold
+      }
       json.dump(data, f)
